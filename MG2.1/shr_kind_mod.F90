d64c851ef9dfc455d3441aefcdafa01a5188d425
ck <ck@cf11e5b4-d6cb-4925-8c48-6fda78d241f6>
2015-03-30 17:52:47 +0000

    Updated version of MG2 based on optimization analysis performed by Chris Kerr

---
+++ MG2.1/shr_kind_mod.F90
@@ -0,0 +1,13 @@
+!===============================================================================
+! SVN $Id: shr_kind_mod.F90 41285 2012-10-26 01:46:45Z sacks $
+! SVN $URL: https://svn-ccsm-models.cgd.ucar.edu/csm_share/branch_tags/share_ece_tags/share_ece_01_140723/shr/shr_kind_mod.F90 $
+!===============================================================================
+
+MODULE shr_kind_mod
+
+   !----------------------------------------------------------------------------
+   ! precision/kind constants add data public
+   !----------------------------------------------------------------------------
+   integer,parameter, public  :: shr_kind_r8 = selected_real_kind(12) ! 8 byte real
+
+END MODULE shr_kind_mod
