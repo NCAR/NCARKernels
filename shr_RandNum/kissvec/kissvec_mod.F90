aed474a2e5a0d4120fac0dcab189806cecac4e63
ck <ck@cf11e5b4-d6cb-4925-8c48-6fda78d241f6>
2015-06-03 21:19:20 +0000

    fixes to initialization of KISSVEC

--- shr_RandNum/kissvec/kissvec_mod.F90
+++ shr_RandNum/kissvec/kissvec_mod.F90
@@ -7,7 +7,7 @@ module kissvec_mod
 implicit none
 
 INTEGER,parameter :: r8 = selected_real_kind(12)
-INTEGER,parameter :: i8 = selected_real_kind(12)
+INTEGER,parameter :: i8 = selected_int_kind(12)
 LOGICAL :: BIG_ENDIAN
 
 private
