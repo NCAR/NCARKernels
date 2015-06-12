50bd9948bb4395d9c9a34c8f040f07fde8f0da32
ck <ck@cf11e5b4-d6cb-4925-8c48-6fda78d241f6>
2015-06-12 13:41:59 +0000

    sean santos's modifications to create c version of kissvec

--- shr_RandNum/kissvec/kissvec_mod.F90
+++ shr_RandNum/kissvec/kissvec_mod.F90
@@ -1,15 +1,10 @@
 module kissvec_mod
-! public domain code
-! made available from http://www.fortran.com/
-! downloaded by pjr on 03/16/04
-! converted to vector form, functions inlined by pjr,mvr on 05/10/2004
+! Fortran binding for the KISS vectorizable random number generator.
 
 implicit none
 
-INTEGER,parameter :: r8 = selected_real_kind(12)
-INTEGER,parameter :: i8 = selected_int_kind(12)
-LOGICAL :: BIG_ENDIAN
-LOGICAL :: initialized = .false.
+integer, parameter :: r8 = selected_real_kind(12)
+integer, parameter :: i4 = selected_real_kind(6)
 
 private
 public :: kissvec
@@ -18,67 +13,30 @@ contains
 
   subroutine kissvec( seed1, seed2, seed3, seed4, ran_arr, length)
 
-! The  KISS (Keep It Simple Stupid) random number generator. Combines:
-! (1) The congruential generator x(n)=69069*x(n-1)+1327217885, period 2^32.
-! (2) A 3-shift shift-register generator, period 2^32-1,
-! (3) Two 16-bit multiply-with-carry generators, period 597273182964842497>2^59
-!  Overall period>2^123;
-!
-    INTEGER,                     INTENT(IN)    :: length
-    REAL(r8), DIMENSION(length), INTENT(INOUT) :: ran_arr
-    INTEGER,  DIMENSION(length), INTENT(INOUT) :: seed1, seed2, seed3, seed4
-    INTEGER :: kiss
-    INTEGER :: sz, k, n, i
+    ! We can assume that an r8 is a double and an i4 is an int32_t, but we can't
+    ! make any guarantees about the relative sizes of a Fortran default integer
+    ! and C's size_t.
+    use iso_c_binding, only: c_size_t
 
-    if ( .NOT. initialized ) then
-      big_endian = (transfer(1_i8, 1) == 0)
-      initialized = .true.
-    endif
+    integer,                     intent(in)    :: length
+    real(r8), dimension(length), intent(inout) :: ran_arr
+    integer(i4),  dimension(length), intent(inout) :: seed1, seed2, seed3, seed4
 
-    if ( big_endian ) then
-      sz = SIZE(seed1)
-      do i=1,sz
-        kiss = 69069*seed1(i) + 1327217885 
-        seed1(i) = transfer(ishft(kiss,bit_size(1)),1)
-        seed2(i) = m (m (m (seed2(i), 13), - 17), 5)
-        seed3(i) = 18000 * iand (seed3(i), 65535) + ishft (seed3(i), - 16)
-        seed4(i) = 30903 * iand (seed4(i), 65535) + ishft (seed4(i), - 16)
-        kiss = seed1(i) + seed2(i) + ishft (seed3(i), 16) + seed4(i)
-        ran_arr(i) = low_byte(kiss)*2.328306e-10_r8 + 0.5_r8
-      enddo
-    else
-      sz = SIZE(seed1)
-      do i=1,sz
-        kiss = 69069*seed1(i) + 1327217885   
-        seed1(i) = kiss
-        seed2(i) = m (m (m (seed2(i), 13), - 17), 5)
-        seed3(i) = 18000 * iand (seed3(i), 65535) + ishft (seed3(i), - 16)
-        seed4(i) = 30903 * iand (seed4(i), 65535) + ishft (seed4(i), - 16)
-        kiss = seed1(i) + seed2(i) + ishft (seed3(i), 16) + seed4(i)
-        ran_arr(i) = kiss*2.328306e-10_r8 + 0.5_r8
-    enddo
-  endif
+    ! C implementation
+    interface
+       subroutine kiss_rng(seed1, seed2, seed3, seed4, ran_arr, length) bind(c)
+         ! Note that the definition of kiss_rng uses unsigned int, but the
+         ! Fortran standard largely ignores signed/unsigned because there
+         ! are no unsigned integers in Fortran.
+         use iso_c_binding, only: c_int32_t, c_double, c_size_t
+         integer(c_int32_t), intent(inout) :: seed1(*), seed2(*), seed3(*), seed4(*)
+         real(c_double), intent(inout) :: ran_arr(*)
+         integer(c_size_t), value :: length
+       end subroutine kiss_rng
+    end interface
+
+    call kiss_rng(seed1, seed2, seed3, seed4, ran_arr, int(length, c_size_t))
 
   end subroutine kissvec
-
-  pure integer function m(k, n)
-    integer, intent(in) :: k
-    integer, intent(in) :: n
-
-    m = ieor (k, ishft (k, n) )
-
-  end function m
-
-  pure integer function low_byte(i)
-  integer, intent(in) :: i
-
-  if (big_endian) then
-    low_byte = transfer(ishft(i,bit_size(1)),1)
-  else
-    low_byte = transfer(i,1)
-  end if
-
-  end function low_byte
-
 
 end module kissvec_mod
