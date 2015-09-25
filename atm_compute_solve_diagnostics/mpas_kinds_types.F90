de4a3cb0a6f3da74ede71fd8aa4311fb19c0d825
johnmauff <dennis@ucar.edu>
2015-09-25 10:28:58 -0600

    add MPAS kernel atm_compute_solve_diagnostics

---
+++ atm_compute_solve_diagnostics/mpas_kinds_types.F90
@@ -0,0 +1,30 @@
+
+! KGEN-generated Fortran source file
+!
+! Filename    : mpas_kinds_types.F90
+! Generated at: 2015-09-25 10:14:07
+! KGEN version: 0.5.0
+
+
+
+    MODULE mpas_kind_types
+        USE kgen_utils_mod, ONLY : kgen_dp, check_t, kgen_init_check, kgen_print_check
+        INTEGER, parameter :: rkind  = selected_real_kind(12)
+        CONTAINS
+
+        ! write subroutines
+        ! No subroutines
+        ! No module extern variables
+        !***********************************************************************
+        !
+        !  routine dummy_kinds
+        !
+        !> \brief   MPAS Dummy kind routine
+        !> \author  Michael Duda
+        !> \date    03/27/13
+        !> \details
+        !> This is a dummy routine that doesn't do anything.
+        !
+        !-----------------------------------------------------------------------
+
+    END MODULE mpas_kind_types
