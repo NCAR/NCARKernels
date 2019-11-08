48edc5855d4229c86d5a701d7d08b4ec8b72bb55
Youngsung Kim <youngsun@ucar.edu>
2019-11-08 15:32:02 -0700

    created v18 folder; working on porting the main loop in micro_mg_tend

---
+++ all/CESM2_MG2/v18/runkernel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+module use /glade/work/cponder/SHARE/Modules/Latest
+module use /glade/work/cponder/SHARE/Modules/Legacy
+
+module use --append /glade/work/cponder/SHARE/Modules/Bundles
+
+for dir in /glade/work/cponder/SHARE/Modules/PrgEnv/*/*
+do
+    module use --append $dir
+done
+
+module purge
+#module load cuda/10.1
+module load PrgEnv/PGI+OpenMPI/2019-04-30 
+module load pgi
+ulimit -s unlimited
+module list
+
+#export PGI_ACC_CUDA_HEAPSIZE=100M
+export PGI_ACC_DEBUG=1
+
+echo $LD_LIBRARY_PATH
+#nvprof --print-gpu-trace ./kernel.exe
+./kernel.exe
+
